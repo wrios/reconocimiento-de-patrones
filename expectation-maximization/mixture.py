3eea08d78a53993df2d1dd7888a2c8c185b06b00
wrios@dc.uba.ar <kennedy.wrc@hotmail.com>
2019-06-24 09:58:10 -0300

    mixtura de gaussianas listo (falta experimentación)

--- expectation-maximization/mixture.py
+++ expectation-maximization/mixture.py
@@ -1,4 +1,5 @@
 import math
+import random
 import numpy as np
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
@@ -31,12 +32,10 @@ def traza_diag(matrix_diag):
 		suma_traza = suma_traza+matrix_diag[diag][diag]
 	return suma_traza
 
-#los centroide iniciales se inician random o con algún algoritmo
-def generar_U_ks(data, cant_clases, cant_features):
+def generar_U_muestrales(data, cant_clases, cant_features):
 	size_cluster = (np.shape(data)[0])/cant_clases
 	dimension = (cant_clases ,cant_features)
-	mus_iniciales = []#np.zeros(dimension)
-	print(np.shape(mus_iniciales))
+	mus_iniciales = []
 	for index_in_class in range(0,cant_clases):
 		mu_mean = np.zeros(cant_features)
 		for iter_clus in range(0,int(size_cluster)):
@@ -49,26 +48,19 @@ def generar_U_ks(data, cant_clases, cant_features):
 	mus_iniciales = mus_iniciales / size_cluster
 	return np.asarray(mus_iniciales)
 
-#las matrices de covarianzas serán los sigmas iniciales
-def generar_Sigma_ks(data, cant_clases, cant_features, mus_iniciales):
+def generar_Sigma_muestrales(data, cant_clases, cant_features, mus_iniciales):
 	size_cluster = (np.shape(data)[0])/cant_clases
 	dimension_x_i = np.shape(data)[1]
 	dimension = (cant_clases ,cant_features)
-	sigmas_iniciales = []#np.zeros(dimension)
-	print(np.shape(sigmas_iniciales))
+	sigmas_iniciales = []
 	for index_in_class in range(0,cant_clases):
 		sigmas_mean = np.zeros((dimension_x_i,dimension_x_i))
 		for iter_clus in range(0,int(size_cluster)):
 			index = iter_clus + (index_in_class*size_cluster)
-			#print("x_i: ",data[int(index)], " shape: ",np.shape(data[int(index)]))
-			#print("mus_i: ",mus_iniciales[index_in_class], " shape: ",np.shape(mus_iniciales[index_in_class]))
 			x_u = data[int(index)] - mus_iniciales[index_in_class]
 			x_u = x_u.reshape(dimension_x_i,1)
-			#print("x-u: ",x_u, " shape: ",np.shape(x_u))
 			temp = np.dot(x_u,x_u.T)
-			#print("x_u * x_u.T: ",temp, " shape: ",np.shape(temp))
 			sigmas_mean = sigmas_mean + temp
-			#print("sigmas_mean: ",sigmas_mean)
 		iter_clus = iter_clus+1
 		sigmas_iniciales.append(sigmas_mean)
 	sigmas_iniciales = np.asarray(sigmas_iniciales)
@@ -90,26 +82,37 @@ def clasificar(x_n, mus):
 	r_n[min_ind] = 1
 	return r_n
 
-def actualizar_mus(mus, data, r_clasificacion):
-	i = 0
-	cantidad_clases = np.shape(mus)[0]
-	cant_clases = np.zeros(cantidad_clases)
-	centroide = np.zeros(np.shape(mus))
-	for r_i in r_clasificacion:
-		j = 0
-		for r_i_j in r_i:
-			if r_i_j != 0:
-				cant_clases[j] = cant_clases[j]+1
-				centroide[j] = centroide[j] + data[i]
-			j = j+1
-		i = i+1
-	for clase in range(0,cantidad_clases):
-		centroide[clase] = centroide[clase] / cant_clases[clase]
-	return np.asarray(centroide)
+def actualizar_mus(data, responsabilidad_n_k):
+	cant_elems = np.shape(data)[0]
+	cant_clases = np.shape(responsabilidad_n_k)[1]
+	dimension_x_i = np.shape(data)[1]
+	cant_by_clases = np.zeros(cant_clases)
+	centroide = np.zeros((cant_clases,dimension_x_i))
+	for fila in range(cant_elems):
+		for col in range(cant_clases):
+			cant_by_clases[col] = cant_by_clases[col]+responsabilidad_n_k[fila][col]
+			centroide[col] = centroide[col] + (data[fila]*responsabilidad_n_k[fila][col])
+	for clase in range(cant_clases):
+		centroide[clase] = centroide[clase] / cant_by_clases[clase]
+	return np.asarray(centroide),np.asarray(cant_by_clases)
     
-def actualizar_sigmas(mus_new, data, r_clasificacion):
-	
-	return 0
+def actualizar_sigmas(mus, data, responsabilidad_n_k,cant_by_clases):
+	cant_elems = np.shape(data)[0]
+	cant_clases = np.shape(responsabilidad_n_k)[1]
+	dimension_x_i = np.shape(data)[1]
+	sigmas = []
+	for mu in mus:
+		sigmas.append(np.zeros((dimension_x_i,dimension_x_i)))
+	for fil in range(cant_elems):
+		for col in range(cant_clases):
+			x_diff = data[fil]-mus[col]
+			x_diff = x_diff.reshape(dimension_x_i,1)
+			sigmas_n_k = np.dot(x_diff, x_diff.T)
+			sigmas_n_k = sigmas_n_k * responsabilidad_n_k[fil][col]
+			sigmas[col] = sigmas[col] + sigmas_n_k
+	for k in range(cant_clases):
+		sigmas[k] = sigmas[k]/cant_by_clases[k]
+	return np.asarray(sigmas)
 
 def aplanar_clasificacion(r_result):
 	clasificacion = []
@@ -136,11 +139,7 @@ def generar_pis(cant_elems, cant_clases):
 	for index_class in range(0, cant_clases):
 		cant_elems_by_class = cant_elems/cant_clases
 		vec_pi.append(cant_elems_by_class/cant_elems)
-	print(vec_pi)
 	return asarray(vec_pi)
-
-def mixture_error(N_new, pis_new):
-	return 0
 
 def f_n(x_i, pis, mus, sigmas, cant_clases, cant_features):
 	N_i = []
@@ -151,120 +150,82 @@ def f_n(x_i, pis, mus, sigmas, cant_clases, cant_features):
 		x_u = x_i - mus[clase]
 		temp = np.dot(x_u, inversa)
 		temp = np.dot(temp,x_u.T)
-		exp = pow(math.e,(-0.5)*temp)*pis[clase]
+		exp = pow(math.e,(-0.5)*temp)
 		N_i.append(exp / divisor)
 	return asarray(N_i)
 
-def generar_N(data, pis, mus, sigmas, cant_clases, cant_features):
+def actualizar_N(data, pis, mus, sigmas, cant_clases, cant_features):
 	matrix_n = []
 	for x_i in data:
 		matrix_n.append(f_n(x_i, pis, mus, sigmas, cant_clases, cant_features))
 	return asarray(matrix_n)
 
-def actualizar_gamma(N, pis):
+def actualizar_responsabilidades(N, pis):
 	pis = pis.reshape(cant_clases,1)
 	gamma = []
-	for N_i in range(0,np.shape(N)[0]):
+	for N_i in range(np.shape(N)[0]):
 		gamma_i = []
-		for N_i_j in range(0,np.shape(N)[1]):
+		for N_i_j in range(np.shape(N)[1]):
 			temp = N[N_i].reshape(cant_clases,1)
-			#print("shape: N[N_i]",np.shape(temp.T))
-			#print("shape: pis",np.shape(pis))
-			multiplier = (N[N_i][N_i_j]*pis[N_i_j])/np.dot(temp.T,pis)
-			#print("shape: append",multiplier[0][0])
-			gamma_i.append(multiplier[0][0])
+			num = (N[N_i][N_i_j]*pis[N_i_j])[0]
+			div = np.dot(temp.T,pis)[0][0]
+			multiplier = num/div
+			gamma_i.append(multiplier)
 		gamma.append(gamma_i)
 	return asarray(gamma)
+
+def m_error(N, pis, cant_elems):
+	error = 0
+	for N_i in N:
+		error = error + math.log10(np.dot(N_i,pis))
+	return error/cant_elems
+
+def clasificar_data(data, mus):
+	z_clasificacion = []
+	for d in data:
+		temp = clasificar(d, mus)
+		z_clasificacion.append(temp)
+	z_clasificacion = np.asarray(z_clasificacion)
+	return aplanar_clasificacion(z_clasificacion)
+
 ###Mixturas Gaussianas
 
 def gaussian_mixture(data, acept_error, cant_elems, cant_clases, cant_features):
-	mus_old = generar_U_ks(data, cant_clases, cant_features)
-	print("primeros mus: ", mus_old)
-	sigmas_old = generar_Sigma_ks(data, cant_clases, cant_features, mus_old)
-	print("primeros sigmas: ", sigmas_old)
-	pis_old = generar_pis(cant_elems, cant_clases)
-	print("primeros pis: ", pis_old)
-	itercion = 0
-	z_old = []
-	error = infinity
-	N_old = generar_N(data, pis_old, mus_old, sigmas_old, cant_clases, cant_features)
-	print("primeros N's: ", np.shape(N_old))
-	gammas_old = actualizar_gamma(N_old, pis_old)
-	print("primeros gamma's: ", np.shape(gammas_old))
-	while error > acept_error:
-#Definir N y gamma
-#actualizar la clasificación
-		mus_new = []
-		r_new = []
-		sigmas_new = []
-		pis_new = []
-		N_new = []
-		gammas_new = []
-		itercion = itercion+1
-#actualización de mus
-		print("iteracion: ", itercion)
-		for d in data:
-			temp = clasificar(d, mus_old)
-			r_new.append(temp)
-		r_new = np.asarray(r_new)
-		#aca ya se clasificarón los datos
-		mus_new = actualizar_mus(mus_old, data, r_new)
-		mus_new = np.asarray(mus_new)
-		print("mus old: ",np.asarray(mus_old))
-		print("mus new: ",np.asarray(mus_new))
-		print("r_clasificacion: ", z_old)
-		z_old = r_new
-		mus_old = mus_new
-#actualización de sigmas
-#actualización de π's
-#calcular error
-		error = mixture_error(N_new, pis_new)
-		print("error actual: ", error)
-	clasificacion = aplanar_clasificacion(z_old)
-	print("sale por: r no cambio")
+	max_iter = 9
+	mus = generar_U_muestrales(data, cant_clases, cant_features)
+	sigmas = generar_Sigma_muestrales(data, cant_clases, cant_features, mus)
+	pis = generar_pis(cant_elems, cant_clases)
+	iteracion = 0
+	error = acept_error+1
+	while(iteracion < max_iter):
+		iteracion = iteracion+1
+		N_old = actualizar_N(data, pis, mus, sigmas, cant_clases, cant_features)
+		responsabilidad_n_k = actualizar_responsabilidades(N_old, pis)
+		mus,N_k = actualizar_mus(data, responsabilidad_n_k)
+		pis_new = N_k/cant_elems
+		sigmas = actualizar_sigmas(mus, data, responsabilidad_n_k, N_k)
+		error = m_error(N_old, pis,cant_elems)
+		print("error: ", error)
+		if error < acept_error:
+			clasificacion = clasificar_data(data, mus)
+			print("sale por error: ", error," < ",acept_error)
+			return clasificacion
+	clasificacion = clasificar_data(data, mus)
+	print("sale por iteracion : ", iteracion," < ",max_iter)
 	return clasificacion
-
-
-#1#
-##definimos los primeros elementos generando batchs de N/k elementos
-
-##definir las π_k, μ_k y Σ_k iniciales
-
-
-#2#
-##N = [N(X_1)_t..N(X_n)_t]
-##N(X_i) = [N(X_i|U_1,Σ_1)..N(X_i|U_k,Σ_k)]
-##γ(Z_ij) = (N[i][j]*π[j])/(N[i]*π)
-#γ= (γ(x_1),..,γ(x_N))_t
-#γ(x_i) = (z_i1,..z_ik)
-#primero calcular π_k N(x|μ_k,Σ_k) k in {1,..,K}
-###Expectation:
-#μ_k = (sum{n=1..N}γ(z_nk)x_n) / N_k
-#γ(z_k)= π_k N(x|μ_k,Σ_k) / sum{j=1,..,k}[π_j N(x|μ_j,Σ_j)].
-def responsabilidad(x_n, mus, sigmas):
-	return 0
-
-
-
-###Comienza la experimentación
-
 
 
 # load the iris dataset 
 iris = datasets.load_iris()
-
-# select first two columns  
 X = iris.data[:, :]
 
 d = pd.DataFrame(X) 
 data = dataFrame_to_NumpyArray(d)
+data = data[:, :2]
 cant_clases = 3
-print("cantidad de clases: ", cant_clases)
 cant_features = np.shape(data)[1]
-print("cantidad de features: ", cant_features)
 cant_elems = np.shape(data)[0]
-print("cantidad de elems: ", cant_elems)
-acept_error = 0.1 
+acept_error = -1.25
 labels = gaussian_mixture(data, acept_error, cant_elems, cant_clases, cant_features)
 d['labels']= labels 
 d0 = d[d['labels']== 0] 
@@ -273,4 +234,5 @@ d2 = d[d['labels']== 2]
 plt.scatter(d0[0], d0[1], c ='r') 
 plt.scatter(d1[0], d1[1], c ='yellow') 
 plt.scatter(d2[0], d2[1], c ='g') 
-#plt.show()
+plt.show()
+###Comienza la experimentación
